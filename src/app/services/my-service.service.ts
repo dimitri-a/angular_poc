05f1f2d08a05a0911f84166dabb076eb439dceaf
Dimitri C <couwenberg@gmail.com>
2019-05-22 17:07:59 +1000

    trying to create service

---
+++ src/app/services/my-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyServiceService {
+
+  constructor(private http:HttpClient) { }
+
+  getData():Observable<any[]>{
+    return this.http.get<any[]>('http://localhost:3000/getplayers')
+  }
+}
